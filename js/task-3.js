f56fe7111b1295786fb8f2eec7fe8351ed1de97a
Hakan Keleş <hakankkeles@gmail.com>
2025-05-05 12:44:30 +0300

    metod'lar da this kullanımı düzeltildi

--- js/task-3.js
+++ js/task-3.js
@@ -8,7 +8,7 @@ const profile = {
         this.playTime += saatler;
     },    
     getInfo(){
-        return `${profile.username} has ${profile.playTime} active hours!`
+        return `${this.username} has ${this.playTime} active hours!`
     }
 };
 
