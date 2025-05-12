a59a7423034494c89bc0744225e6cc61a6c28e5e
Hakan Keles <hakankkeles@gmail.com>
2025-05-12 23:35:46 +0300

    son hata düzenlemesi

--- js/task-3.js
+++ js/task-3.js
@@ -4,8 +4,8 @@ const profile = {
     changeUsername(newName){
         this.username= newName; 
     },
-    updatePlayTime(saatler){
-        this.playTime += saatler;
+    updatePlayTime(hours){
+        this.playTime += hours;
     },    
     getInfo(){
         return `${this.username} has ${this.playTime} active hours!`
@@ -23,4 +23,4 @@ profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
